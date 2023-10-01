f6f39eb4ea822a371a22d83af7bcbd1abc33ecfd
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-01 12:56:15 +0530

    Add files via upload

---
+++ if else if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter a number ";
+	cin>>n;
+	if(n>0){
+		cout<<"This is a Positive integer";
+	
+	}
+	else if(n==0){
+		cout<<"Zero is even integer";
+	}
+	else{
+		cout<<"This is a Negative integer"<<endl;
+		
+	}
+	return 0;
+}
